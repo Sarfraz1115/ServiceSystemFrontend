343588e027dc330f2ce94571322a31afa1b29a97
Sarfraz1115 <syedsarfraz907@gmail.com>
2025-08-29 14:42:22 +0530

    backend added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://servicesystembackend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
